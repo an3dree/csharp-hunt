6413df5f52d962407694a8694fd7f8991d79009a
André Souza <an3dree@gmail.com>
2024-05-13 10:18:17 -0300

    first commit

---
+++ dotnet-hunt/simple-pressk-macro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using WindowsInput;
+
+class Program
+{
+    static void Main()
+    {
+        bool running = true;
+        while (running)
+        {
+            Timer timer = new Timer(PressKey, null, 30000, Timeout.Infinite);
+            Console.WriteLine("Esperando 30 segundos");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            if (keyInfo.Key == ConsoleKey.Q)
+            {
+                running = false;
+            }
+            //Console.ReadLine();
+        }
+
+
+    }
+
+    static void PressKey(object state)
+    {
+        InputSimulator inputSimulator = new InputSimulator();
+        inputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.VK_A);
+        Console.WriteLine("Tecla A pressionada!");
+    }
+}
